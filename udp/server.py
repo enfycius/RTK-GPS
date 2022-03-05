79d10a6134f2e8f1db91b5c6c35357c5b93397ed
김종혁 <kimjonghyeok@gimjonghyeog-ui-MacBookPro.local>
2022-03-05 12:32:44 +0900

    fix #5

---
+++ udp/server.py
@@ -0,0 +1,19 @@
+import socket, sys
+
+port = 80
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+host = socket.gethostbyname('0.0.0.0')
+
+s.bind((host, port))
+
+while True:
+    data, addr = s.recvfrom(1024)
+    print(data)
+
+    if not data:
+        break
+
+s.close()
+sys.exit()
