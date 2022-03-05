4ff66d99a2c6d99e218b06af6d7a5b4bc8cc12ce
Kim JongHyeok <34566999+enfycius@users.noreply.github.com>
2022-03-05 16:40:18 +0900

    Revert "Feature/issue #8 send"

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/com/Documents/rtk-gps
+# Install script for directory: /home/hexk/Downloads/RTK-GPS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/com/Documents/rtk-gps/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/hexk/Downloads/RTK-GPS/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
